cc971338307d92f03270075944e34a9d7e009853
Ismul Aksan <44281550+Ismulaksan@users.noreply.github.com>
2019-07-21 11:44:48 +0700

    Add files via upload

---
+++ Minggu 01-02/Latihan/Object and array.js
@@ -0,0 +1,24 @@
+function sumSalaries(salaries) {
+
+    let sum = 0;
+    for (let salary of Object.values(salaries)) {
+      sum += salary;
+    }
+  
+    return sum; // 650
+  }
+  
+  let salaries = {
+    "John": 100,
+    "Pete": 300,
+    "Mary": 250
+  };
+  
+ document.write( sumSalaries(salaries) ); // 650
+
+  let user = {
+    name: 'John',
+    age: 30
+  };
+  
+  document.write( count(user) ); // 2
